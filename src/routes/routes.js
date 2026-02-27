29e4556f234376d16dab0ddb5d4ef94db84730c9
hieule210924tb <manu210924@gmail.com>
2026-02-27 14:40:05 +0700

    add content for page About Us

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -21,5 +21,9 @@ const routers = [
         path: '/product/:id',
         component: lazy(() => import('@pages/DetailProduct')),
     },
+    {
+        path: '/about-us',
+        component: lazy(() => import('@pages/AboutUs')),
+    },
 ];
 export default routers;
